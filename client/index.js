e30032450b1a8cc2e262c3b8923acbf603227619
Rebecca Chan <chan.rebecca@gmail.com>
2018-12-11 17:41:58 -0500

    basic set up fixed

--- client/index.js
+++ client/index.js
@@ -1,21 +1,32 @@
-
-console.log('is this working')
-
+import Player from '@vimeo/player';
 
 
+const playertwo = new Player('playertwo', {
+  url: "https://vimeo.com/76979871",
+  width: 640
+})
+
+playertwo.on('play', function () {
+  console.log('player two played!')
+})
 
 
-
+// An interface for the user to add Cues at certain timestamps.
+//     - Should also list the Cues that have been added and allow them to be deleted.
+// - While playing the video surface the Cues at the correct times and hide after a duration.
+// - A Cue only needs to contain a string.
+// - The messages should be displayed as an overlay on the player itself.
+// - Only use vanilla javascript and please write all your own CSS.
 //ajx
-const ajax = async () => {
-  try {
-    const result = await fetch('/api');
-    const data = await result.json();
-    console.log(data);
-  }
-  catch (error) {
-    console.error(error);
-  }
-}
+// const ajax = async () => {
+//   try {
+//     const result = await fetch('/api');
+//     const data = await result.json();
+//     console.log(data);
+//   }
+//   catch (error) {
+//     console.error(error);
+//   }
+// }
 
-ajax();
+// ajax();
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  // the starting point for our program
   entry: './client/index.js',
 
-  // affects several default webpack settings
   mode: 'development',
 
   output: {
-    // The absolute path for the directory where we want the output to be placed.
-    // Whenever we need an absolute path, it's a best practice to use the built-in
-    // `path` module
     path: path.join(__dirname, '/public'),
-
-    // The name of the file that will contain our output.
-    // We could name this whatever we want, but bundle.js is typical
     filename: 'bundle.js'
   },
 
