23c6e5b3343007cf6eea640dac2d70e1df3cd860
Rebecca Chan <chan.rebecca@gmail.com>
2018-12-13 20:58:42 -0500

    added time in seconds

--- client/index.js
+++ client/index.js
@@ -7,53 +7,108 @@ const playertwo = new Player('playertwo', {
 })
 
 playertwo.on('play', function () {
-  console.log('player two played without scriptsrc!')
+  console.log('player two played')
 })
 
+playertwo.on('timeupdate', async function () {
+  const currentTime = await this.getCurrentTime();
+  // function formatTime(time) {
+  //   var hrs = ~~(time / 3600);
+  //   var mins = ~~((time % 3600) / 60);
+  //   var secs = ~~time % 60;
+  //   var ret = "";
+  //   if (hrs > 0) {
+  //     ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+  //   }
+  //   ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+  //   ret += "" + secs;
+  //   return ret;
+  // }
+  // const formattedTime = formatTime(currentTime)
+  const formattedTime = Math.round(currentTime)
+  document.getElementById('current').innerHTML = `Current Time In Seconds: ${formattedTime}`
+})
+
+//activeCues gets populated by form with cue objects
+let activeCues = []
+
+function displayCues(list) {
+  console.log('cuepoints activeCuesList', list)
+  list.forEach((cue) => {
+    console.log('each cue', cue)
+    console.log('currentTime', currentTime)
+    console.log('formatted time', formattedTime)
+    if (cue.time === currentTime) {
+      console.log(cue.data.text)
+    }
+  })
+}
+
+displayCues(activeCues)
 
 
-const activeCues = []
 
-//i want to display current time in case the uer wants to know where he/she is. will probably have to throttle because the rate is too ridic
-// const currentTime = playertwo.getCurrentTime()
-// console.log(currentTime)
-// const currentTimeDisplay = document.getElementById('cue-time-stamp')
-// currentTimeDisplay.className = 'cue-time-stamp'
-// currentTimeDisplay.innerHTML = currentTime
+//foreach element in activecues, if time on player === element.time, then display element.data.text
+
+
+// Cuepoint
+// if (data && 'time' in data && 'data' in data) {
+//   return apiLog(eventName + ' event (' + data.time + ')', getFormattedMessage(data.data), {
+//       className: 'event',
+//       preFormatted: true
+//   });
+// }
+//listenfor time update
+// player.on('timeupdate', function(data) {
+//   currentTimeRange.val(data.seconds);
+// });
+
 
 
 //we should be adding cue overlays to the video player here
 
-
+//adds event listener to cue submit Form
 document.getElementById('cue-form').addEventListener('submit', event => {
   event.preventDefault();
-  const cueText = document.getElementById('cue-text').value
+  let cueText = document.getElementById('cue-text').value
   console.log('cueText', cueText)
-  const cueTime = document.getElementById('cue-time-stamp').value
+  let cueTime = document.getElementById('cue-time-stamp').value
   console.log('cueTime', cueTime)
   makeCue(cueText, cueTime)
+
+
+  //reset form
+  document.getElementById('cue-text').value = ''
+  document.getElementById('cue-time-stamp').value = ''
 })
 
 
-//makes Cue object after submit button is clicked!
+//define makeCue function
 let cueCounter = 0
 const makeCue = (cueText, cueTime) => {
+  //create removeButton
   const removeButton = document.createElement('button')
   removeButton.className = 'remove-btn'
   removeButton.innerHTML = 'x'
 
+  //create cue list item with id
   cueCounter++;
   const cueListItem = document.createElement('li')
   cueListItem.className = 'cue-list-item'
   cueListItem.id = cueCounter
-  const newCueText = document.createElement('p')
+
+  //create text inside cue list item
+  const newCueText = document.createElement('span')
   newCueText.innerHTML = cueText
-  const newCueTime = document.createElement('p')
+  //adds time-stamp inside cue list item
+  const newCueTime = document.createElement('span')
   newCueTime.innerHTML = `:${cueTime}secs `
+
+  //appends text, time-stamp, and removeButton in list item
   cueListItem.append(newCueTime, newCueText, removeButton)
   document.getElementById('cues-list').append(cueListItem)
-  console.log('before push', activeCues)
 
+  //creates cue object for Player to read
   const newCue = {
     time: cueTime,
     data: {
@@ -62,18 +117,22 @@ const makeCue = (cueText, cueTime) => {
     id: cueListItem.id
   }
 
+  //adds cue object to activeCues array
   activeCues.push(newCue)
+
+  //add removeButton Listener
+  removeButton.addEventListener('click', function remove() {
+    //remove event listener
+    removeButton.removeEventListener('click', remove)
+    //remove cue from list
+    activeCues = activeCues.filter(selected => selected.id !== cueListItem.id)
+    //remove cue from the DOM
+    cueListItem.remove();
+  })
+
 }
 console.log('after push', activeCues)
 
-//cue object looks like this:
-// {
-//   "time": 15,
-//   "data": {
-//       "customKey": "customValue"
-//   },
-//   "id": "09ecf4e4-b587-42cf-ad9f-e666b679c9ab"
-// }
 
 
 // An interface for the user to add Cues at certain timestamps.
