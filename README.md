e4e456525ecca20a77d64c4bba79e601002d6988
Rebecca Chan <chan.rebecca@gmail.com>
2018-12-14 17:56:27 -0500

    Vimeo Challenge Complete

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # Rebecca Chan's Vimeo Coding Challenge
 
-## Set Up
 
-### MacOS/Linux
 
 * `npm install`
-* `npm run-build`
+* `npm run build`
 
+
+This Vimeo Cue Editor is created as a Single Page Application using only vanilla JS
+
+Allows user to add cues at specific times - by seconds
+Shows added cues to allow removal
+
+Cues are displayed as an overlay on the player as text
+Used own styling, but used sample as a guide.
+
+Did not use native API Player methods for cue Activity
+
+Allows user to change video based off ID search
 
--- client/index.js
+++ client/index.js
@@ -1,19 +1,25 @@
 import Player from '@vimeo/player';
 
-const playertwo = new Player('playertwo', {
+//Create instance of Vimeo Player
+const player = new Player('player', {
   url: "https://vimeo.com/76979871",
 })
 
-playertwo.on('timeupdate', async function () {
+//Adds timeupdate listener
+player.on('timeupdate', async function () {
+
+  //Display current time in seconds
   const currentTime = await this.getCurrentTime();
-  const formattedTime = Math.round(currentTime)
+  const formattedTime = ~~(currentTime)
   document.getElementById('current').innerHTML = `Current Time In Seconds: ${formattedTime}`
 
+  //Displays cue overlay if a cue time matches player current time
   function displayCues(activeCuesList) {
     activeCuesList.forEach(function (cue) {
       if (Number(cue.time) === ~~(currentTime)) {
         document.getElementById('cue-overlay-text').innerHTML = cue.data.text
       }
+      //Cue disappears after 5 seconds
       if (currentTime - cue.time > 5) {
         document.getElementById('cue-overlay-text').innerHTML = ''
       }
@@ -22,77 +28,76 @@ playertwo.on('timeupdate', async function () {
   displayCues(activeCues)
 })
 
-//activeCues gets populated by form with cue objects
+//Active Cues array to store all our cue instances
 let activeCues = []
 
-//adds event listener to cue submit Form
+//Add event listener and makeCue function to Add Cue button
 document.getElementById('cue-form').addEventListener('submit', event => {
   event.preventDefault();
-  let cueText = document.getElementById('cue-text').value
+  let cueTextEl = document.getElementById('cue-text')
+  let cueText = cueTextEl.value
   let cueTime = document.getElementById('cue-time-stamp').value
   makeCue(cueText, cueTime)
 
-
-  //reset form
+  //Resets form to be blank and places cursor back into form
   document.getElementById('cue-text').value = ''
   document.getElementById('cue-time-stamp').value = ''
+  cueTextEl.focus()
 })
 
 
-//define makeCue function
+//Let's make a cue!
 let cueCounter = 0
 const makeCue = (cueText, cueTime) => {
-  //create removeButton
+  //First create a remove button for that recently made cue
   const removeButton = document.createElement('button')
   removeButton.className = 'remove-btn'
   removeButton.innerHTML = '✕'
 
-  //create cue list item with id
+  //Create a Cue List Item with a unique ID
   cueCounter++;
   const cueListItem = document.createElement('div')
   cueListItem.className = 'cue-list-item'
   cueListItem.id = cueCounter
 
-  //create text inside cue list item
+  //Add input text to this Cue List Item
   const newCueText = document.createElement('span')
   newCueText.innerHTML = cueText
   newCueText.className = 'text'
-  //adds time-stamp inside cue list item
+  //Add input time-stamp to Cue List Item
   const newCueTime = document.createElement('span')
   newCueTime.className = 'time'
   newCueTime.innerHTML = `${formatTime(cueTime)} `
 
-  //formats time appearance into hh:mm:ss in cues list
+  //Format time-stamp appearance into hh:mm:ss
   function formatTime(time) {
     const hrs = ~~(time / 3600);
     const mins = ~~((time % 3600) / 60);
     const secs = ~~time % 60;
-    let ret = "";
+    let newTime = "";
     if (hrs > 0) {
-      ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+      newTime += "" + hrs + ":" + (mins < 10 ? "0" : "");
     }
-    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
-    ret += "" + secs;
-    return ret;
+    newTime += "" + mins + ":" + (secs < 10 ? "0" : "");
+    newTime += "" + secs;
+    return newTime;
   }
 
-  // appends text, time-stamp, and removeButton in list item
+  // Appends child text, time-stamp, and removeButton to list item
   cueListItem.append(newCueText, newCueTime, removeButton)
   document.getElementById('cues-list').append(cueListItem)
 
-  //creates cue object for Player to read
+  //Define cue object for Player to read
   const newCue = {
     time: cueTime,
-    data: {
-      text: cueText
-    },
+    data: { text: cueText },
     id: cueListItem.id
   }
 
-  //adds cue object to activeCues array
+  //Add recently created cue object to activeCues array
   activeCues.push(newCue)
 
-  //add removeButton Listener, removes event listener, and then removes it from the DOM and our activeCues array
+  //Add remove button listener for click, once clicked - removes event listener, and then removes it from the DOM and our activeCues array
   removeButton.addEventListener('click', function remove() {
     removeButton.removeEventListener('click', remove)
     activeCues = activeCues.filter(selected => selected.id !== cueListItem.id)
@@ -100,20 +105,24 @@ const makeCue = (cueText, cueTime) => {
   })
 }
 
+//Change Video
+document.getElementById('search').addEventListener('click', event => {
+  //get input Id from the DOM
+  const newVideoEl = document.getElementById('video-url-changer')
+  const newVideoId = newVideoEl.value
+  const currentVideo = document.getElementById('player')
 
+  //set data-vimeo-url attribute to new URL and load new video
+  currentVideo.setAttribute('data-vimeo-url', `https://vimeo.com/${newVideoId}`)
+  player.url = `https://vimeo.com/${newVideoId}`
+  player.loadVideo(newVideoId)
 
-//you could do an ajax call into the vimeo api - maybe api by video id??
-// you would need a separate cues db for persistence
-// const ajax = async () => {
-//   try {
-//     const result = await fetch('/api');
-//     const data = await result.json();
-//     console.log(data);
-//   }
-//   catch (error) {
-//     console.error(error);
-//   }
-// }
+  //Housekeeping - remove all objects from activeCues array and remove them from the DOM
+  activeCues = []
+  const removeCueItems = (cues) => [...cues].forEach(cue => cue.remove())
+  removeCueItems(document.querySelectorAll(".cue-list-item"))
 
-// ajax();
+  //Clear cue overlay in case one is displayed while video is switching
+  document.getElementById('cue-overlay-text').innerHTML = ''
+})
 
